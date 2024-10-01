daded53ee9a6fe11b7804317a07fd81c95f6f95e
nyebat <mojoid7@gmail.com>
2024-10-02 00:13:44 +0700

    update

--- termux.sh
+++ termux.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+setup_termux() {
+    # setup color scheme
+    theme="https://github.com/dracula/termux/archive/master.zip"
+    curl -L -o ~/dracula.zip $theme
+    unzip -o ~/dracula.zip -d ~/
+    cp -r ~/termux-master/colors.properties ~/.termux/colors.properties
+    rm -rf ~/{termux-master,dracula.zip}
+
+    # setup font
+    font="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip"
+    curl -L -o ~/font.zip $font
+    unzip -o ~/font.zip -d ~/
+
+    cp -r ~/0xProtoNerdFont-Regular.ttf ~/.termux/font.ttf
+    rm -rf ~/{font.zip,*.ttf,README.md,LICENSE}
+
+    [ ! -d "$HOME/storage" ] && termux-setup-storage
+
+}
+
 setup_nvim() {
     [ ! -d ~/.config ] && mkdir -p .config
     git clone https://github.com/nyebat/nvim-dotfiles ~/dotfiles
-    rm -rfv ~/dotfiles/{.git/,README.md}
-    cp -rv  ~/dotfiles/. ~/ && rm -rf ~/{dotfiles,termux.sh}
+    rm -rf ~/dotfiles/{.git/,README.md}
+    cp -r  ~/dotfiles/. ~/ && rm -rf ~/{dotfiles,termux.sh}
 }
 
 install_packages() {
@@ -29,25 +49,6 @@ install_packages() {
     pkg install -y "${packages[@]}"
 }
 
-setup_termux() {
-    # setup termux color scheme
-    theme="https://github.com/dracula/termux/archive/master.zip"
-    curl -L -o ~/dracula.zip $theme
-    unzip -ov ~/dracula.zip -d ~/
-    cp -r ~/termux-master/colors.properties ~/.termux/colors.properties
-    rm -rf ~/{termux-master,dracula.zip}
-
-    # setup termux font
-    font="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip"
-    curl -L -o ~/font.zip $font
-    unzip -ov ~/font.zip -d ~/
-
-    cp -r ~/0xProtoNerdFont-Regular.ttf ~/.termux/font.ttf
-    rm -rf ~/{font.zip,*.ttf,README.md,LICENSE}
-
-    [ ! -d "$HOME/storage" ] && termux-setup-storage
-
-}
 
 set_fish_as_default() {
     shell="fish"
@@ -60,10 +61,11 @@ set_fish_as_default() {
 }
 
 
+setup_termux
 setup_nvim
 install_packages
-setup_termux
 set_fish_as_default
 
+termux-reload-settings
 echo "Setup dan instalasi selesai!"
 
