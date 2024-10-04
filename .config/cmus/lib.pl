2a77d0fb49d6b96968fee75c0c718634e05ab732
nyebat <mojoid7@gmail.com>
2024-10-05 03:28:52 +0700

    up

--- .config/cmus/lib.pl
+++
@@ -1,5 +0,0 @@
-/storage/emulated/0/Download/YTDLnis/Audio/Wesh Dipdip - Kamu nyata with Lyrics Acoustic - Izzy - (Ost. D'bijis).flac
-/storage/emulated/0/Download/YTDLnis/Audio/WALI SONGO - NING UMI LAILA.flac
-/storage/emulated/0/Download/YTDLnis/Audio/DPM Dangdut Station - Novi Andarista - Ya Badrotim (Official Music Video).flac
-/storage/emulated/0/Download/YTDLnis/Audio/SLUKU SLUKU BATHOK - NING UMI LAILA.flac
-/storage/emulated/0/Download/YTDLnis/Audio/Stars and Rabbit - Like It Here.flac
